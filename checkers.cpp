4aa7bd8b1111ac67fa42cd227302905bc0e5ae3f
Adam <adamkupka05s@gmail.com>
2023-01-11 18:55:22 +0100

    ruch czarnym hetmanem

--- checkers.cpp
+++ checkers.cpp
@@ -11,23 +11,7 @@ int main()
     game.board.board[0][1].fieldState = field_empty;
     game.board.board[7][0].fieldState = field_empty;
     game.board.board[7][6].fieldState = field_empty;
-
-    for (int i = 0;i < 8;i++) {
-        for (int j = 0; j < 8;j++) {
-            game.board.board[i][j].fieldState = field_empty;
-        }
-    }
-
-   
-    game.board.board[3][1].fieldState = field_white;
-    game.board.board[5][1].fieldState = field_black;
-    game.board.board[6][2].fieldState = field_black;
-    game.board.board[7][3].fieldState = field_black;
-
-  
-   
-
-   
+ 
     while (game.gameEnd() == false) {
         game.move();
     }
